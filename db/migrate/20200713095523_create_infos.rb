5d725d39cd28ea18f46d38dc7b0f6199ca64dbe4
taiga-tech <taiga.dev.n@gmail.com>
2020-07-13 19:04:29 +0900

    update info migrate add column

--- db/migrate/20200713095523_create_infos.rb
+++ db/migrate/20200713095523_create_infos.rb
@@ -1,7 +1,12 @@
 class CreateInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :infos do |t|
-      
+      t.integer :code,    null: false
+      t.string :address,  null: false
+      t.integer :tel,     null: false
+      t.string :weekday,  null: false
+      t.string :weekend,  null: false
+      t.string :holiday,  null: false
       t.timestamps
     end
   end
