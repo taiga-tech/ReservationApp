37e7c79c90ddb884fe7ba714af0c7bec9f32c99b
taiga-tech <taiga.dev.n@gmail.com>
2020-07-31 17:17:51 +0900

    add seed, cvs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/table-infos.csv') do |row|
+  Info.create(code: row[1], address: row[2], tel: row[3], weekday: row[4], weekend: row[5], holiday: row[6])
+end
+
+CSV.foreach('db/table-calculations.csv') do |row|
+  Calculation.create(tax: row[1], discount: row[2])
+end
+
+CSV.foreach("db/table-categories.csv") do |row|
+  Category.create(name: row[1])
+end
+
+CSV.foreach('db/table-menus.csv') do |row|
+  Menu.create(name: row[1], price: row[2], comment: row[3])
+end
+
+CSV.foreach('db/table-menu_categories.csv') do |row|
+  MenuCategory.create(menu_id: row[1], category_id: row[2])
+end
