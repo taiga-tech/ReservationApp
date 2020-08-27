fb3c004103fc66af35cfd21cb2d8d233fb55d733
taiga-tech <taiga.dev.n@gmail.com>
2020-08-27 21:37:38 +0900

    fix other

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require rails-ujs
-//= require_tree .
-// = require popper
-// = require bootstrap-sprockets
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery-ui/widgets/sortable
 //= require rails_sortable
+//= require_tree .
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,7 +1,7 @@
 class Staff < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  validates :name, presence: true
 end
